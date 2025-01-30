acb5c5d862321b5a0ab6dbdf9f46c2259cbcf8c4
Areeba Abid Mahmood <areebaabid.h@gmail.com>
2025-01-30 13:02:25 -0500

    Book Hub Project

--- app/src/main/java/com/example/bookhub/Components/BookCard.kt
+++ app/src/main/java/com/example/bookhub/Components/BookCard.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -13,6 +15,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -54,7 +57,7 @@ fun BookCard(
                 contentDescription = title,
                 modifier = Modifier
                     .size(100.dp)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp).align(Alignment.CenterVertically)
             )
 
             // Book Details
@@ -65,13 +68,19 @@ fun BookCard(
                 Text(
                     text = title ?: "Untitled",
                     style = MaterialTheme.typography.titleLarge,
-                    maxLines = 1
+                    maxLines = 2
                 )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
                 Text(
                     text = authors.joinToString(", "),
                     style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 1
+                    maxLines = 2
                 )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
                 Text(
                     text = description ?: "No Description Available",
                     style = MaterialTheme.typography.bodySmall,
